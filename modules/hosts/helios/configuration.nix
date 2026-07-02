e972aa94f1057596cf1c9557a5eb7cf4aaec5389
robotic-rabbit <anirudha.py@gmail.com>
2026-07-03 03:51:37 +1000

    disabled tlp because of conflict with gnome power daemon

--- modules/hosts/helios/configuration.nix
+++ modules/hosts/helios/configuration.nix
@@ -107,15 +107,15 @@
     environment.systemPackages = with pkgs; [
     ];
 
-    services.tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_BOOST_ON_AC = 0;
-        CPU_BOOST_ON_BAT = 0;
-      };
-    };
+    # services.tlp = {
+      # enable = true;
+      # settings = {
+        # CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        # CPU_BOOST_ON_AC = 0;
+        # CPU_BOOST_ON_BAT = 0;
+      # };
+    # };
 
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
